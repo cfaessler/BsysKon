36880d0a26adb15341d67fcaec368be193f6e35f
cfaessler <cfaessle@hsr.ch>
2014-03-03 23:10:30 +0100

    fixed write to named pipe bug, added 2 sec delay

--- Uebung02/Aufgabe04/Aufgabe04.c
+++ Uebung02/Aufgabe04/Aufgabe04.c
@@ -6,38 +6,55 @@
 
 
 int main(){
-    char * rx_pipe = "input";
-    char * tx_pipe = "output";
+    char * rx_pipe = "/tmp/ser_tx";
+    char * tx_pipe = "/tmp/ser_rx";
     
+    unlink(rx_pipe);
+    unlink(tx_pipe);
+
     if(mkfifo(rx_pipe, S_IRWXU)){ 
-        printf("ERROR creating rx pipe");
+        printf("[Server]: ERROR creating rx pipe");
+        return 1;
     }
 
     if(mkfifo(tx_pipe, S_IRWXU)){
-        printf("ERROR creating tx pipe");
+        printf("[Server]: ERROR creating tx pipe");
+        return 1;
     }
 
     int rx_fd = open(rx_pipe, O_RDWR);
 
     if(rx_fd==-1){
-        printf("ERROR opening rx pipe");
+        printf("[Server]: ERROR opening rx pipe");
+        return 1;
     }
     
     int tx_fd = open(tx_pipe, O_RDWR);
 
     if(tx_fd==-1){
-        printf("ERROR opening tx pipe");
+        printf("[Server]: ERROR opening tx pipe");
+        return 1;
     }
     
     char buf[42];
     int bytes_received = read(rx_fd, buf, sizeof(buf));
     if(bytes_received<=0){
-        printf("ERROR reading rx pipe");
+        printf("[Server]: ERROR reading rx pipe");
+        return 1;
     }
+
+    printf("[Server]: %i bytes received\n", bytes_received);
 
     int bytes_written = write(tx_fd, buf, bytes_received);
     if(bytes_written<=0){
-        printf("ERROR writing tx pipe");
+        printf("ERROR writing tx pipe\n");
+        return 1;
     }
 
+    printf("[Server]: %i bytes written\n", bytes_written);
+    sleep(2);
+    close(tx_fd);
+    close(rx_fd);
+    unlink(tx_pipe);
+    unlink(rx_pipe);
 }
